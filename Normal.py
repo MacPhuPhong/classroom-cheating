0b868d05e7536fca922b371029df9deb44b720a0
MacPhuPhong <phongmac0101@gmail.com>
2025-09-08 17:31:59 +0700

    update: tự đẩy ảnh đã cắt về thư mục normal, tối ưu 1 giai đoạn

--- Normal.py
+++ Normal.py
@@ -1,12 +1,12 @@
-import os
+import os 
 import cv2
-from ultralytics import YOLO
+from  ultralytics import YOLO
 import pandas as pd
 
-# Load your YOLO model
+# yolo model
 model = YOLO("yolo11s-pose.pt")
 
-# Video path
+# video path 
 cap = cv2.VideoCapture('vid_output.mp4')
 
 # Get video properties
@@ -14,47 +14,45 @@ frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 fps = cap.get(cv2.CAP_PROP_FPS)
 seconds = round(frames / fps)
 
-frame_total = 2000
-i = 0
-a = 6008
+frame_total = 1000
+i = 0 
+a = 0
 
 all_data = []
 
 while cap.isOpened():
-    # Set the position in milliseconds
-    cap.set(cv2.CAP_PROP_POS_MSEC, (i * ((seconds / frame_total) * 1000)))
+    # set the position in milliseconds
+    cap.set(cv2.CAP_PROP_POS_MSEC, (i * ((seconds/frame_total)*1000)))
     flag, frame = cap.read()
-
     if not flag:
         break
-
-    # Save full frame image
-    pa = r'D:\git&github\classroom-cheating\images'
+    # save full frame image
+    pa = r'D:\git&github\classroom-cheating\dataset_path\images'
     image_path = f'{pa}\img_{i}.jpg'
     cv2.imwrite(image_path, frame)
-
-    # Run YOLO detection
+    
+    #run yolo detection
     results = model(frame, verbose=False)
-
+    
     for r in results:
-        bound_box = r.boxes.xyxy  # Get bounding boxes
-        conf = r.boxes.conf.tolist()  # Confidence score
-        keypoints = r.keypoints.xyn.tolist()  # Human keypoints
-
+        bound_box = r.boxes.xyxy  # get bounding boxes
+        conf = r.boxes.conf.tolist()  # confidence score
+        keypoints = r.keypoints.xyn.tolist()  # human keypoints
+        
         for index, box in enumerate(bound_box):
             if conf[index] > 0.75:
                 x1, y1, x2, y2 = box.tolist()
                 cropped_person = frame[int(y1):int(y2), int(x1):int(x2)]
-                op = r'D:\git&github\classroom-cheating\images1'
+                op = r'D:\git&github\classroom-cheating\dataset_path\Normal'
                 output_path = f'{op}\person_nn_{a}.jpg'
-
+                
                 data = {'image_name': f'person_nn_{a}.jpg'}
-
-                # Save keypoint data
+                
+                # save keypoint data
                 for j in range(len(keypoints[index])):
                     data[f'x{j}'] = keypoints[index][j][0]
                     data[f'y{j}'] = keypoints[index][j][1]
-
+                
                 all_data.append(data)
                 cv2.imwrite(output_path, cropped_person)
                 a += 1
@@ -67,13 +65,12 @@ cv2.destroyAllWindows()
 # Convert to DataFrame
 df = pd.DataFrame(all_data)
 
-# Path to your CSV file
-csv_file_path = r'D:\git&github\classroom-cheating\nkeypoint.csv'
+csv_file_path = r'D:\git&github\classroom-cheating\dataset_path\normal_keypoint.csv'   
 
-# Check if the file exists to determine whether to append or create new
+
 if not os.path.isfile(csv_file_path):
-    df.to_csv(csv_file_path, index=False)  # Create new file if it doesn't exist
+    df.to_csv(csv_file_path, index=False)
 else:
-    df.to_csv(csv_file_path, mode='a', header=False, index=False)  # Append if it exists
-
+    df.to_csv(csv_file_path, mode='a', header=False, index=False)
+    
 print(f"Keypoint data saved to {csv_file_path}")
